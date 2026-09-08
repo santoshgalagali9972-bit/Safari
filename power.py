d6cb85a3ac3f780684621ca35b91b077b802a9f2
Santosh <santoshgalagali9972@gmail.com>
2026-09-09 00:15:50 +0530

    exponenet

---
+++ power.py
@@ -0,0 +1,9 @@
+base = float(input("enter base: "))
+exponent = float(input("Enter exponent: "))
+
+if exponent >= 0:
+    result = base ** exponent
+    print("Result: ",result)
+
+else:
+    print("neg")
